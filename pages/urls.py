bf308d5dad3953407eac2e54964f6c3d2c05bbb0
NoPYXN <yunfournes@gmail.com>
2025-10-06 16:59:04 +0200

    ajout de la page d'accueil

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.accueil, name='accueil'),
+    path('joueurs/', views.joueurs, name='joueurs'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def accueil(request):
     return render(request, 'pages/accueil.html')
+
+def joueurs(request):
+    return render(request, 'joueurs.html')
