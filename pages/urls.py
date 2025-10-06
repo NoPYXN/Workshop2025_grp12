e8b58f94dc8c46b40b1da07428536feaaac83305
unknown <lyonel_ag@outlook.fr>
2025-10-06 16:59:25 +0200

    selection joueur

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.accueil, name='accueil'),
+    #path('', views.accueil, name='accueil'),
+    path('', views.joueurs, name='joueurs'),
+    path('j1.html', views.j1, name='j1'),
+    path('j2.html', views.j2, name='j2'),
+    path('j3.html', views.j3, name='j3'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def accueil(request):
     return render(request, 'pages/accueil.html')
+
+def joueurs(request):
+    return render(request, 'pages/joueurs.html')
+
+def j1(request):
+    return render(request, 'pages/j1.html')
+
+def j2(request):
+    return render(request, 'pages/j2.html')
+
+def j3(request):
+    return render(request, 'pages/j3.html')
