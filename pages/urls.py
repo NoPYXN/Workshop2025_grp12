84ce6cd5e4836e3282f052449e88208005541063
NoPYXN <yunfournes@gmail.com>
2025-10-07 10:19:16 +0200

    merge + correctif

--- pages/urls.py
+++ pages/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.accueil, name='accueil'),
-    path('joueurs/', views.joueurs, name='joueurs'),
+    path('', views.accueil, name='accueil'),
+    path('joueurs.html>', views.joueurs, name='joueurs'),
     path('j1.html', views.j1, name='j1'),
     path('j2.html', views.j2, name='j2'),
     path('j3.html', views.j3, name='j3'),
