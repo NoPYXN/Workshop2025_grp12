37a6727b34462444205251c15f5b1057e05460eb
SartSilver25 <lyonel_ag@outlook.fr>
2025-10-09 16:33:30 +0200

    modif victoire.html

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.accueil, name='accueil'),
     path('', views.index, name='index'),
     path('joueurs.html>', views.joueurs, name='joueurs'),
     path('question1/<int:joueur_id>/', views.question1, name='question1'),
